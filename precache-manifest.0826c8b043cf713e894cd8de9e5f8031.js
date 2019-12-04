f990d8cd505386b0fdb6fbfa2a9e60c45138cce4
Mark <s.markmas@gmail.com>
2019-12-05 01:12:30 +0530

    Updates

---
+++ precache-manifest.0826c8b043cf713e894cd8de9e5f8031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236c7e186630f264da8ad6a8706c7b9e",
+    "url": "/coffee/index.html"
+  },
+  {
+    "revision": "c6c9d4c3d26dd22e5b44",
+    "url": "/coffee/static/css/main.bef7aaef.chunk.css"
+  },
+  {
+    "revision": "67cba28bca0b5aa32c79",
+    "url": "/coffee/static/js/2.3d73fa76.chunk.js"
+  },
+  {
+    "revision": "c6c9d4c3d26dd22e5b44",
+    "url": "/coffee/static/js/main.9313fbac.chunk.js"
+  },
+  {
+    "revision": "8c67ec5905d22cb6417a",
+    "url": "/coffee/static/js/runtime-main.e9c5b8ec.js"
+  }
+]);
